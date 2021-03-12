19f585bb8905182c5d821ccc18b893d81e52a20e
Anu Sharma <sharmaanu2002in@gmail.com>
2021-03-12 21:04:33 +0530

    Added files

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
+import { Component } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component  {
+  
+  pressingButton2 = () =>{
+      alert('Called a function')
+  }
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Welcome! Anu Sharma's app.  </Text>
+        <Text style = {{color: 'black'}}> How are you doing today?</Text>
+        <Button title = "Post" color = "black" />
+        <TouchableOpacity style = {{height:50, paddingHorizontal : 10, backgroundColor: 'yellow', justifyContent:'center', alignItems: 'center',
+        
+      }} onPress = {() => this.pressingButton2()}> 
+          <Text style = {{fontSize : 25}}> Submit </Text>
+        </TouchableOpacity>
+
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
